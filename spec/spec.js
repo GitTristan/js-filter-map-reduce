5ea99583f9dfbc82ce2632c1947a66b8f01a040a
Andreas Kavountzis <akavount@gmail.com>
2015-05-01 01:14:25 -0600

    Spec and source files for JS Filter, Map, Reduce exercises

---
+++ spec/spec.js
@@ -0,0 +1,87 @@
+var yourCode = require('../src');
+
+function isEven(n) {
+  return n % 2 == 0;
+}
+
+describe('#compact', function() {
+  it('returns a new array with all provided undefined values removed', function() {
+    var input = [undefined, 1,2,3, null, 'foo', 'bar'];
+    expect(yourCode.compact(input)).toEqual([1,2,3, null, 'foo', 'bar']);
+  });
+
+  it('returns an empty array if all provided values are undefined', function() {
+    var input = [undefined, undefined, undefined];
+    expect(yourCode.compact(input)).toEqual([]);
+  });
+});
+
+describe('#all', function() {
+  it('returns false if any  element in the provided array does not matches the condition given by the function parameter', function() {
+    var input = [1,3,5,6,7];
+    expect(yourCode.all(input, isEven)).toBe(false);
+  });
+
+  it('returns true if every element in the provided array matches the condition given by the function parameter', function() {
+    var input = [2,4,6,8];
+    expect(yourCode.all(input, isEven)).toBe(true);
+  });
+});
+
+describe('#allRecursive', function() {
+  it('returns false if any  element in the provided array does not matches the condition given by the function parameter', function() {
+    var input = [1,3,5,6,7];
+    expect(yourCode.allRecursive(input, isEven)).toBe(false);
+  });
+
+  it('returns true if every element in the provided array matches the condition given by the function parameter', function() {
+    var input = [2,4,6,8];
+    expect(yourCode.allRecursive(input, isEven)).toBe(true);
+  });
+});
+
+describe('#any', function() {
+  it('returns true if any element in the provided array matches the condition given by the function parameter', function() {
+    var input = [1,3,5,6,7];
+    expect(yourCode.any(input, isEven)).toBe(true);
+  });
+
+  it('returns false if no element in the provided array matches the condition given by the function parameter', function() {
+    var input = [1,3,5,7,9];
+    expect(yourCode.any(input, isEven)).toBe(false);
+  });
+});
+
+describe('#anyRecursive', function() {
+  it('returns true if any element in the provided array matches the condition given by the function parameter', function() {
+    var input = [1,3,5,6,7];
+    expect(yourCode.anyRecursive(input, isEven)).toBe(true);
+  });
+
+  it('returns false if no element in the provided array matches the condition given by the function parameter', function() {
+    var input = [1,3,5,7,9];
+    expect(yourCode.anyRecursive(input, isEven)).toBe(false);
+  });
+});
+
+describe('#partition', function() {
+  it('creates a new array whose first element is an array containing all positive matches for the provided comparison function and whose second element is an array containing all negative matches', function() {
+    var input = [1,2,3,4,5,6];
+    expect(yourCode.partition(input, isEven)).toEqual([[2,4,6], [1,3,5]]);
+  });
+
+  it('returns two empty arrays in an array when given an empty array', function() {
+    var input = [];
+    expect(yourCode.partition(input, isEven)).toEqual([[], []]);
+  });
+});
+
+describe('#reduce', function() {
+  it('behaves like Array#reduce but accepts the array as the first argument', function() {
+    expect(yourCode.reduce([1,2,3], function(sum, inc) { return sum + inc; }, 100)).toBe(106);
+  });
+
+  it('works without an initial value by taking the first array element as the initial value', function() {
+    expect(yourCode.reduce([1,2,3], function(sum, inc) { return sum + inc; })).toBe(6);
+  });
+});
---
+++ src.js
@@ -0,0 +1,28 @@
+module.exports = {
+  compact: function(array) {
+    // use `filter` to write this method
+  },
+
+  all: function(array, condition) {
+    // use `reduce` to write this method
+  },
+
+  allRecursive: function(array, condition) {
+    // write a recursive version of the `all` method
+  },
+
+  any: function(array, condition) {
+    // use `reduce` to write this method
+  },
+
+  anyRecursive: function(array, condition) {
+    // write a recursive version of the `any` method
+  },
+
+  partition: function(array, condition) {
+    // use `reduce` to write this method
+  }
+
+  // You will need to add the reduce method yourself, obviously
+  // your implementation should not call Array#reduce.
+};
